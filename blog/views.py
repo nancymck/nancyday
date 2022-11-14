8cc7b61387a1013d9eeb042ce138d2dac183a76e
Nancy McKelvey <nancymckelvey@Nancys-iMac.lan>
2022-11-14 23:31:50 +1000

    go away

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from datetime import datetime
 
 # Create your views here.
+
+def TodayView(request):
+
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    current_day = datetime.now()
+    A = current_day.strftime('%A %-d')
+    B = current_day.strftime('%B %Y')
+    time = current_day.strftime('%H:%M')
+    title = str()
+
+    if current_day.day > 3:
+        title = A+'th ' + B
+    else:
+        if current_day.day == 1:
+            title = A+ 'st ' + B
+        if current_day.day == 2:
+            title = A+ 'nd ' + B
+        if current_day.day == 3:
+            title = A+ 'rd ' + B
+
+    context = {'title': title, 'time': time}
+
+    return render(request, 'today.html', context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'blog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Brisbane'
 
 USE_I18N = True
 
